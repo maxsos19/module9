7980e5496b2397d90c712d3124ea106fe55026c9
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-05-03 22:26:47 +0300

    Add files via upload

---
+++ Program_9_6_task1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract
+{
+    class CustomException : Exception
+    {
+        public CustomException(string message) : base(message) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = {
+            new IndexOutOfRangeException("Выход за пределы массива"),
+            new ArgumentNullException("Попытка обратиться к null объекту"),
+            new DivideByZeroException("Ошибочное деление на ноль"),
+            new ArgumentException("Неверный аргумент"),
+            new CustomException("Собственное исключение")
+        };
+
+            foreach (var exception in exceptions)
+            {
+                try
+                {
+                    throw exception;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Исключение: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
